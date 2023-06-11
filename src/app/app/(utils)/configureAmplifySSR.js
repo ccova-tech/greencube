10ecedc798ff4c5fba029f60c8ab79255a377843
Carlos Cova <ing.geologo@gmail.com>
2023-06-11 18:51:45 -0400

    fix: importación de aws-exports en configureAmplifySSR

--- src/app/app/(utils)/configureAmplifySSR.js
+++ src/app/app/(utils)/configureAmplifySSR.js
@@ -1,4 +1,5 @@
 import { Amplify } from 'aws-amplify'
-import awsExports from '@/aws-exports.js'
+// import awsExports1 from '@/aws-exports.js'
+import awsExports from '../../../aws-exports'
 
 Amplify.configure({...awsExports, ssr:true})
