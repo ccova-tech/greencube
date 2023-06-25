95f8a640f5ed44fd26573633acc42e7fa1eda7bd
Carlos Cova <ing.geologo@gmail.com>
2023-06-25 11:47:47 -0400

    fix: link menu app

--- src/app/app/page.jsx
+++ src/app/app/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import Link from 'next/link'
 import { MenuContext } from './layout';
 import { ApiOutlined, EnvironmentOutlined, SmileOutlined, SyncOutlined } from '@ant-design/icons'
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div className='sm:h-full flex flex-wrap justify-center items-center gap-10 sm:bg-primary rounded-lg'>
 
-      <Link href='/app/registro' key='registro' onClick={(e) => {e.preventDefault(); setItemMenu('registro')}}>
+      <Link href='/app/registro' key='registro' onClick={() => setItemMenu('registro')}>
         <div className='group w-64 h-80 hover:shadow-card transition-all cursor-pointer bg-secondary rounded-lg overflow-hidden border border-solid border-secondary'>
           <div className='h-2/3 flex flex-col justify-center items-center'>
             <ApiOutlined className='text-7xl text-destacado group-hover:text-primary'/>
@@ -23,7 +23,7 @@ export default function App() {
         </div>
       </Link>
 
-      <Link href='/app/recoleccion' key='recoleccion' onClick={(e) => {e.preventDefault(); setItemMenu('recoleccion')}}>
+      <Link href='/app/recoleccion' key='recoleccion' onClick={() => setItemMenu('recoleccion')}>
         <div className='group w-64 h-80 hover:shadow-card transition-all cursor-pointer bg-secondary rounded-lg overflow-hidden border border-solid border-secondary'>
           <div className='h-2/3 flex flex-col justify-center items-center'>
             <EnvironmentOutlined className='text-7xl text-destacado group-hover:text-primary'/>
@@ -35,7 +35,7 @@ export default function App() {
         </div>
       </Link>
 
-      <Link href='/app/mispuntos' key='mispuntos' onClick={(e) => {e.preventDefault(); setItemMenu('mispuntos')}}>
+      <Link href='/app/mispuntos' key='mispuntos' onClick={() => setItemMenu('mispuntos')}>
         <div className='group w-64 h-80 hover:shadow-card transition-all cursor-pointer bg-secondary rounded-lg overflow-hidden border border-solid border-secondary'>
           <div className='h-2/3 flex flex-col justify-center items-center'>
             <SmileOutlined className='text-7xl text-destacado group-hover:text-primary'/>
@@ -47,7 +47,7 @@ export default function App() {
         </div>
       </Link>
 
-      <Link href='/app/reciclaje' key='reciclaje' onClick={(e) => {e.preventDefault(); setItemMenu('reciclaje')}}>
+      <Link href='/app/reciclaje' key='reciclaje' onClick={() => setItemMenu('reciclaje')}>
         <div className='group w-64 h-80 hover:shadow-card transition-all cursor-pointer bg-secondary rounded-lg overflow-hidden border border-solid border-secondary'>
           <div className='h-2/3 flex flex-col justify-center items-center'>
             <SyncOutlined className='text-7xl text-destacado group-hover:text-primary'/>
